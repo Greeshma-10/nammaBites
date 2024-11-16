e082d6734a9191b81ab13ea1b47267d3dff5ccb6
Greeshma V <greeshmavamannaik@gmail.com>
2024-11-16 15:39:20 +0530

    added store data and style.css

---
+++ js/store-data.js
@@ -0,0 +1,90 @@
+var stores = [
+    {
+      name: 'Bangalore Palace',
+      address: 'Bangalore Palace, Bengaluru, Karnataka',
+      phone: '+91 1234567890',
+      lat: 12.9984,
+      lng: 77.5921,
+      image: 'img/bangalore_palace.jpg'
+    },
+    {
+      name: 'Cubbon Park',
+      address: 'Cubbon Park, Bengaluru, Karnataka',
+      phone: '+91 9876543210',
+      lat: 12.9762,
+      lng: 77.5932,
+      image: 'img/cubbon_park.jpg'
+    },
+    {
+      name: 'Lalbagh Botanical Garden',
+      address: 'Lalbagh, Bengaluru, Karnataka',
+      phone: '+91 80 2553 3161',
+      lat: 12.9496,
+      lng: 77.5848,
+      image: 'img/lalbag.jpg'
+    },
+    {
+      name: 'Vidhana Soudha',
+      address: 'Vidhana Soudha, Bengaluru, Karnataka',
+      phone: '+91 80 2235 8335',
+      lat: 12.9798,
+      lng: 77.5908,
+      image: 'img/vidhan_soudha.webp'
+    },
+    {
+      name: 'ISKCON Temple',
+      address: 'ISKCON Temple, Rajajinagar, Bengaluru, Karnataka',
+      phone: '+91 80 2347 3437',
+      lat: 13.0103,
+      lng: 77.5554,
+      image: 'img/iskon.jpg'
+    },
+    {
+      name: 'Bannerghatta National Park',
+      address: 'Bannerghatta National Park, Bengaluru, Karnataka',
+      phone: '+91 80 2785 7359',
+      lat: 12.7461,
+      lng: 77.5894,
+      image: 'img/bannergattaPark.jpg'
+    },
+    {
+      name: 'UB City Mall',
+      address: 'UB City Mall, Vittal Mallya Road, Bengaluru, Karnataka',
+      phone: '+91 80 4178 4040',
+      lat: 12.9699,
+      lng: 77.6009,
+      image: 'img/ubcity.jpg'
+    },
+    {
+      name: 'Nandi Hills',
+      address: 'Nandi Hills, Bengaluru Rural, Karnataka',
+      phone: '+91 80 2775 1044',
+      lat: 13.4200,
+      lng: 77.6830,
+      image: 'img/nandi_hills.jpg'
+    },
+    {
+      name: 'Vishweshwaraya Museum',
+      address: 'Vishweshwaraya Museum, Kasturba Road, Bengaluru, Karnataka',
+      phone: '+91 80 2286 7777',
+      lat: 12.9756,
+      lng: 77.6059,
+      image: 'img/vivesvaraya.jpg'
+    },
+    {
+      name: 'Jawaharlal Nehru Planetarium',
+      address: 'Jawaharlal Nehru Planetarium, Bengaluru, Karnataka',
+      phone: '+91 80 2286 2222',
+      lat: 12.9712,
+      lng: 77.5931,
+      image: 'img/planetorium.jpg'
+    },
+    {
+      name: 'Bangalore International Exhibition Center (BIEC)',
+      address: 'BIEC, Bengaluru, Karnataka',
+      phone: '+91 80 6676 4343',
+      lat: 13.0720,
+      lng: 77.5964,
+      image: 'img/beic.jpg'
+    }
+];
